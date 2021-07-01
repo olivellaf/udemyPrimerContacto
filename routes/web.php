c4b89a444d9ce9cb9b61cde12cb335155c7302e8
Ferran Olivella <ferry87@gmail.com>
2021-07-02 01:21:35 +0200

    Conditionals on blade

--- routes/web.php
+++ routes/web.php
@@ -41,7 +41,7 @@ Route::any('/contact', function () {
 ** Use "?" if want to make the parameter NOT required
 ** Use "where" for trying to "validate" if will be required.
 */
-Route::get('/product/{id?}/{type?}', function ( $id = '2500', $type = 'simple' ) {
+Route::get('/product/{id?}/{type?}', function ( $id = '2500', $type = null ) {
     /*
     return view('product', array(
         'id' => $id,
