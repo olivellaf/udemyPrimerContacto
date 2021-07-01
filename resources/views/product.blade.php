010d84b9fbd80d92c28233ba14d57dd6439257dd
Ferran Olivella <ferry87@gmail.com>
2021-07-01 02:35:02 +0200

    Advanced routes

---
+++ resources/views/product.blade.php
@@ -0,0 +1,1 @@
+<h3>This is the product page of #{{ $id }} and is type: {{ $type }}</h3>
--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,32 @@ Route::get('/hola-mundo', function () {
     return 'Hello World';
 });
 
+/* TRYING POST METHOD */
+Route::post('/hola-mundo', function () {
+    return 'Hello World by POST';
+});
+
 Route::get('/contact', function () {
     return view('contact');
 });
+
+/* Use MATCH and tell them which methods do you want that this URL responds,
+** or use ANY for the route works with all http methods
+*/
+Route::any('/contact', function () {
+    return view('contact');
+});
+
+/* Send variables
+** Use "?" if want to make the parameter NOT required
+** Use "where" for trying to "validate" if will be required.
+*/
+Route::any('/product/{id?}/{type?}', function ( $id = '2500', $type = 'simple' ) {
+    return view('product', array(
+        'id' => $id,
+        'type' => $type,
+    ));
+})->where([
+    'id' => '[0-9]+',
+    'type' => '[A-Za-z]+'
+]);
